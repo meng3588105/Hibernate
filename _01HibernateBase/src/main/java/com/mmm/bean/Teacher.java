568689cddceac6fed0a414309a040b763e52d709
MxL358810517 <290966858@qq.com>
2018-05-28 10:47:00 +0800

    第一次上--传增删改查

---
+++ _01HibernateBase/src/main/java/com/mmm/bean/Teacher.java
@@ -0,0 +1,43 @@
+package com.mmm.bean;
+
+/**
+ * Teacher 表对应的实体类
+ */
+public class Teacher {
+
+    private Integer id;//教师编号
+    private String name;//教师姓名
+
+    public Teacher(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Teacher() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
